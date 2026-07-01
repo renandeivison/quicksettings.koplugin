b995b2bcd39a91f170aace79242ba88e140c700b
renandeivison <renandeivison@gmail.com>
2026-07-01 16:22:29 -0300

    Delete quicksettings.koplugin directory

--- quicksettings.koplugin/_meta.lua
+++
@@ -1,6 +0,0 @@
-return {
-    name = "quicksettings",
-    version = "1.0.0",
-    fullname = "Quick Settings Panel",
-    description = "Adds a quick shortcuts panel and smooth illumination sliders to the top menu.",
-}
