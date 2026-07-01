94f6142f340c2019ff0530a59c0b57e7aae50b14
renandeivison <renandeivison@gmail.com>
2026-07-01 16:22:14 -0300

    Add files via upload

---
+++ _meta.lua
@@ -0,0 +1,6 @@
+return {
+    name = "quicksettings",
+    version = "1.1.0",
+    fullname = "Quick Settings Panel",
+    description = "Adds a quick shortcuts panel and smooth illumination sliders to the top menu.",
+}
